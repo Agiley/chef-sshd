dd4f25b63fde146f414f85726c28c486b188f52d
Sebastian Johnsson <sebastian@agiley.se>
2014-05-01 18:15:03 +0200

    Need to conditionally use Upstart as a provider for Ubuntu 14.04

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,14 @@
-service "ssh" do
-  supports [:start, :restart, :stop]
-end
-
-template "/etc/ssh/sshd_config" do
+template "ssh" do
+  path "/etc/ssh/sshd_config"
   source "sshd_config.erb"
   owner "root"
   group "root"
   mode 0644
-  notifies :restart, resources(:service => "ssh"), :delayed
+end
+
+service "ssh" do
+  supports [:start, :restart, :stop]
+  action [:enable]
+  provider Chef::Provider::Service::Upstart if (node["platform"] == "ubuntu" && node["platform_version"].to_f >= 9.10)
+  subscribes :restart, "template[ssh]", :immediately
 end
